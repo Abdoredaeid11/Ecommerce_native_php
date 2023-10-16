5f05070774f758b87c5113a6468c2b46a4b80df6
Abdoredaeid11 <90568504+Abdoredaeid11@users.noreply.github.com>
2023-10-16 14:45:36 +0300

    home page theme

--- includes/footer.php
+++ includes/footer.php
@@ -33,3 +33,11 @@
 <script src="assets/dist/js/pages/dashboard.js"></script>
 <!-- AdminLTE for demo purposes -->
 <script src="assets/dist/js/demo.js"></script>
+
+
+<script src="assets/themes/js/jquery.js" type="text/javascript"></script>
+	<script src="assets/themes/js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="assets/themes/js/google-code-prettify/prettify.js"></script>
+	
+	<script src="assets/themes/js/bootshop.js"></script>
+    <script src="assets/themes/js/jquery.lightbox-0.5.js"></script>
